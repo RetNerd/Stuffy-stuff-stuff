1eddf137f94815e1dc140b4b1b8bcc8908626b80
RetardedNerd <raczmarton292@gmail.com>
2018-12-11 23:56:12 +0100

    Modified to use the new example generator.

--- tf_keras_xor.py
+++ tf_keras_xor.py
@@ -3,17 +3,21 @@
 #from keras.optimizers import SGD
 import tensorflow as tf
 import numpy as np
+from randomdatagenerator import generatedata
 # Define the input and the expected output
-X = np.array([[0,0,0,0],[0,0,0,1],[0,0,1,0],[0,0,1,1],[0,1,0,0],[0,1,0,1],[0,1,1,0],[0,1,1,1],[1,0,0,0],
-[1,0,0,1],[1,0,1,0],[1,0,1,1],[1,1,0,0],[1,1,0,1],[1,1,1,0],[1,1,1,1]])
+X = np.array([[0,0,0,0],[0,0,0,1],[0,0,1,0],[0,0,1,1],
+			  [0,1,0,0],[0,1,0,1],[0,1,1,0],[0,1,1,1],
+			  [1,0,0,0],[1,0,0,1],[1,0,1,0],[1,0,1,1],
+			  [1,1,0,0],[1,1,0,1],[1,1,1,0],[1,1,1,1]])
 y = np.array([[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[0],[1],[1],[1]])
 # The Sequential model is a linear stack of layers.
+IN, OUT = generatedata(X,y, 100)
 model = tf.keras.models.Sequential()
 # The model needs to know what input shape it should expect. For this reason, the first layer in a  Sequential model (and only the first, because following layers can do automatic shape inference) needs to receive information about its input shape.
 # Some 2D layers, such as Dense, support the specification of their input shape via the argument input_dim
-model.add(tf.keras.layers.Dense(4,input_dim = 4 , activation=tf.nn.sigmoid))
-model.add(tf.keras.layers.Dense(5, activation = tf.nn.sigmoid))
-model.add(tf.keras.layers.Dense(1))
+model.add(tf.keras.layers.Dense(4 ,input_shape = (4, ) , activation=tf.nn.relu))
+#model.add(tf.keras.layers.Dense(5, activation = tf.nn.sigmoid))
+model.add(tf.keras.layers.Dense(1, activation = tf.nn.sigmoid))
 
 # Before training a model, you need to configure the learning process, which is done via the compile method
 # It receives three arguments:
@@ -26,7 +30,7 @@ model.add(tf.keras.layers.Dense(1))
 # For any classification problem you will want to set this to metrics=['accuracy'].
 model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
 # Keras models are trained on Numpy arrays of input data and labels. For training a model, you will typically use the fit function.
-model.fit(X, y, batch_size=1, epochs=100)
+model.fit(IN, OUT, batch_size=4, epochs=100)
 model.save('network.hdf5')
 print('Rounded prediction:')
 print(np.round(model.predict_proba(X),2))
